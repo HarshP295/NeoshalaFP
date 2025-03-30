ab9c7f427941191e25b57d5d7918b305daf4c426
HarshP295 <harsh.patilin2005@gmail.com>
2025-03-30 21:20:08 +0530

    Update App.tsx

--- neoshala-homepage/src/App.tsx
+++ neoshala-homepage/src/App.tsx
@@ -9,7 +9,6 @@ import Footer from './components/Footer';
 import './App.css';
 import CourseCards from './components/CourseCards';
 import SignUp from './components/SignUp';
-import PurchasedCourses from './components/purchasedCourses';
 
 const App = () => {
   return (
@@ -20,7 +19,6 @@ const App = () => {
       <SignUp />
       <CourseCards />
       <ContactForm />
-      <PurchasedCourses />
       <Footer />
     </div>
   );
